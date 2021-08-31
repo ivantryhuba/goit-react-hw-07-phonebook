5b645849d0af6f1af2ee455b24404858edcc976d
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-08-31 14:48:56 +0300

    initial commit

---
+++ src/redux/contactsStore.js
@@ -0,0 +1,46 @@
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import contactsReducer from './contactsReducer';
+import logger from 'redux-logger';
+
+const persistConfig = {
+  key: 'contactList',
+  storage,
+};
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const rootReducer = combineReducers({ contactList: contactsReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV === 'development',
+  middleware,
+});
+
+const persistor = persistStore(store);
+
+export default { store, persistor };
